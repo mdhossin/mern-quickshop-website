cff4a04ffd7f0f5cf44dda6d7edf16fccde658ed
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-04-21 20:41:03 +0600

    contact page design is done

---
+++ server/client/src/pages/Contact/GoogleMaps/GoogleMaps.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const GoogleMaps = () => {
+  const containerStyle = {
+    width: "400px",
+    height: "400px",
+  };
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523,
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "YOUR_API_KEY",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  );
+};
+
+export default GoogleMaps;
