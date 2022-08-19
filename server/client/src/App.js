3e61063dfacc1f52d5b55c8e8c9d6142629c6dee
mdhossin <mdsahadathossin778@gmail.com>
2022-08-19 21:11:39 +0600

    added lazy loading

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,39 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, Suspense, lazy } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ToastProvider } from "react-toast-notifications";
-
-import {
-  Header,
-  MyOrders,
-  OrderDetails,
-  Profile,
-  UserDashboard,
-} from "./components";
-import {
-  AboutUs,
-  ActivationEmail,
-  Blog,
-  ConfirmOrder,
-  Contact,
-  ForgotPassword,
-  Home,
-  Login,
-  NotFound,
-  OrderSuccess,
-  Payment,
-  ProductDetail,
-  ProtectedRoute,
-  Register,
-  ResetPassword,
-  Shipping,
-  Shop,
-} from "./pages";
-
-// home pages
-
+import { NotFound, ProtectedRoute } from "./pages";
 import { refreshToken } from "./redux/actions/userActions";
 import "./styles/styles.scss";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Rings } from "react-loader-spinner";
+import { Header } from "./components";
+const MyOrders = lazy(() =>
+  import("./components/Manager/UserDashboard/MyOrders/Myorders")
+);
+const OrderDetails = lazy(() =>
+  import("./components/Manager/UserDashboard/OrderDetails/OrderDetails")
+);
+const Profile = lazy(() =>
+  import("./components/Manager/UserDashboard/Profile/Profile")
+);
+const UserDashboard = lazy(() =>
+  import("./components/Manager/UserDashboard/UserDashboard")
+);
+const AboutUs = lazy(() => import("./pages/AboutUs/AboutUs"));
+
+const ActivationEmail = lazy(() =>
+  import("./pages/ActivationEmail/ActivationEmail")
+);
+const Blog = lazy(() => import("./pages/Blog/Blog"));
+const ConfirmOrder = lazy(() => import("./pages/ConfirmOrder/ConfirmOrder"));
+const Contact = lazy(() => import("./pages/Contact/Contact"));
+const ForgotPassword = lazy(() =>
+  import("./pages/ForgotPassword/ForgotPassword")
+);
+const Home = lazy(() => import("./pages/Home/Home"));
+const Login = lazy(() => import("./pages/Login/Login"));
+const OrderSuccess = lazy(() => import("./pages/OrderSuccess/OrderSuccess"));
+const Payment = lazy(() => import("./pages/Payment/Payment"));
+const ProductDetail = lazy(() => import("./pages/ProductDetail/ProductDetail"));
+const Register = lazy(() => import("./pages/Register/Register"));
+const ResetPassword = lazy(() => import("./pages/ResetPassword/ResetPassword"));
+const Shipping = lazy(() => import("./pages/Shipping/Shipping"));
+const Shop = lazy(() => import("./pages/Shop/Shop"));
+
+// home pages
 
 function App() {
   const dispatch = useDispatch();
@@ -48,84 +56,95 @@ function App() {
       <ToastProvider placement="top-right">
         <BrowserRouter>
           <Header />
+          <Suspense
+            fallback={
+              <Rings
+                ariaLabel="loading-indicator"
+                color="#f3b632"
+                height={100}
+                width={100}
+              />
+            }
+          >
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="register"
+                element={
+                  user?.access_token ? <Navigate to="/" /> : <Register />
+                }
+              />
+              <Route
+                path="forgot"
+                element={
+                  user?.access_token ? <Navigate to="/" /> : <ForgotPassword />
+                }
+              />
+              <Route path="user/reset/:token" element={<ResetPassword />} />
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="login" element={<Login />} /> */}
-            {/* <Route
-              path="register"
-              element={user?.access_token ? <Navigate to="/" /> : <Register />}
-            /> */}
-            {/* <Route
-              path="forgot"
-              element={
-                user?.access_token ? <Navigate to="/" /> : <ForgotPassword />
-              }
-            /> */}
-            {/* <Route path="user/reset/:token" element={<ResetPassword />} /> */}
+              <Route
+                path="active/:activation_token"
+                element={
+                  user?.access_token ? <Navigate to="/" /> : <ActivationEmail />
+                }
+              />
+              <Route path="product/:productId" element={<ProductDetail />} />
+              <Route path="shop" element={<Shop />} />
+              <Route path="about" element={<AboutUs />} />
+              <Route path="blog" element={<Blog />} />
+              <Route path="contact" element={<Contact />} />
 
-            {/* <Route
-              path="active/:activation_token"
-              element={
-                user?.access_token ? <Navigate to="/" /> : <ActivationEmail />
-              }
-            /> */}
-            {/* <Route path="product/:productId" element={<ProductDetail />} /> */}
-            <Route path="shop" element={<Shop />} />
-            <Route path="about" element={<AboutUs />} />
-            <Route path="blog" element={<Blog />} />
-            <Route path="contact" element={<Contact />} />
-            {/* <Route path="modal" element={<ProductDetailModal />} /> */}
+              <Route
+                path="shipping"
+                element={
+                  <ProtectedRoute>
+                    <Shipping />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path="/order/confirm"
+                element={
+                  <ProtectedRoute>
+                    <ConfirmOrder />
+                  </ProtectedRoute>
+                }
+              ></Route>
 
-            {/* <Route
-              path="shipping"
-              element={
-                <ProtectedRoute>
-                  <Shipping />
-                </ProtectedRoute>
-              }
-            ></Route> */}
-            {/* <Route
-              path="/order/confirm"
-              element={
-                <ProtectedRoute>
-                  <ConfirmOrder />
-                </ProtectedRoute>
-              }
-            ></Route> */}
+              <Route
+                path="/process/payment"
+                element={
+                  <ProtectedRoute>
+                    <Payment />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/success"
+                element={
+                  <ProtectedRoute>
+                    <OrderSuccess />
+                  </ProtectedRoute>
+                }
+              />
 
-            {/* <Route
-              path="/process/payment"
-              element={
-                <ProtectedRoute>
-                  <Payment />
-                </ProtectedRoute>
-              }
-            /> */}
-            {/* <Route
-              path="/success"
-              element={
-                <ProtectedRoute>
-                  <OrderSuccess />
-                </ProtectedRoute>
-              }
-            /> */}
+              <Route
+                path="dashboard"
+                element={
+                  <ProtectedRoute>
+                    <UserDashboard />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Profile />} />
+                <Route path="myorders" element={<MyOrders />} />
+                <Route path="order/:orderId" element={<OrderDetails />} />
+              </Route>
 
-            {/* <Route
-              path="dashboard"
-              element={
-                <ProtectedRoute>
-                  <UserDashboard />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Profile />} />
-              <Route path="myorders" element={<MyOrders />} />
-              <Route path="order/:orderId" element={<OrderDetails />} />
-            </Route> */}
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </ToastProvider>
     </>
--- server/client/src/components/index.js
+++ server/client/src/components/index.js
@@ -1,55 +1,24 @@
-import Header from "./common/Header/Header";
-import CartList from "./common/CartList/CartList";
-import CartSummary from "./common/CartSummary/CartSummary";
-import CheckOut from "./common/Checkout/Checkout";
-import HeroSlide from "./common/HeroSlide/HeroSlide";
-import SupportArea from "./common/SupportArea/SupportArea";
-import DiscountBanner from "./common/DiscountBanner/DiscountBanner";
-import Products from "./common/Products/Products";
-import Iphone from "./common/Iphone/Iphone";
-import Brands from "./common/Brands/Brands";
-import NewsLetter from "./common/NewsLetter/NewsLetter";
-import AppDownload from "./common/AppDownload/AppDownload";
+export { default as Header } from "./common/Header/Header";
+export { default as CartList } from "./common/CartList/CartList";
+export { default as CartSummary } from "./common/CartSummary/CartSummary";
+export { default as CheckOut } from "./common/Checkout/Checkout";
+export { default as HeroSlide } from "./common/HeroSlide/HeroSlide";
+export { default as SupportArea } from "./common/SupportArea/SupportArea";
+export { default as DiscountBanner } from "./common/DiscountBanner/DiscountBanner";
+export { default as Products } from "./common/Products/Products";
+export { default as Iphone } from "./common/Iphone/Iphone";
+export { default as Brands } from "./common/Brands/Brands";
+export { default as NewsLetter } from "./common/NewsLetter/NewsLetter";
+export { default as AppDownload } from "./common/AppDownload/AppDownload";
 
-import Footer from "./common/Footer/Footer";
+export { default as Footer } from "./common/Footer/Footer";
 // loading
-import Loading from "./common/Loading/Loading";
-import ProductRating from "./common/ProductRating/ProductRating";
-
-// user dashbaord
-import UserDashboard from "./Manager/UserDashboard/UserDashboard";
-import Profile from "./Manager/UserDashboard/Profile/Profile";
-import MyOrders from "./Manager/UserDashboard/MyOrders/Myorders";
-import OrderDetails from "./Manager/UserDashboard/OrderDetails/OrderDetails";
+export { default as Loading } from "./common/Loading/Loading";
+export { default as ProductRating } from "./common/ProductRating/ProductRating";
 
 /// about us
-import OurHistory from "./common/OurHistory/OurHistory";
-import OurFeatures from "./common/OurFeatures/OurFeatures";
+export { default as OurHistory } from "./common/OurHistory/OurHistory";
+export { default as OurFeatures } from "./common/OurFeatures/OurFeatures";
 
 //
-import WishItemList from "./common/WishItemList/WishItemList";
-
-export {
-  Header,
-  CartList,
-  CartSummary,
-  CheckOut,
-  HeroSlide,
-  SupportArea,
-  DiscountBanner,
-  Loading,
-  ProductRating,
-  Products,
-  Iphone,
-  Brands,
-  NewsLetter,
-  AppDownload,
-  Footer,
-  UserDashboard,
-  Profile,
-  MyOrders,
-  OrderDetails,
-  OurHistory,
-  OurFeatures,
-  WishItemList,
-};
+export { default as WishItemList } from "./common/WishItemList/WishItemList";
