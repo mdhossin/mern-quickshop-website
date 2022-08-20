581fdff771ff826c676c5bc419be9b9808ad39b7
mdhossin <mdsahadathossin778@gmail.com>
2022-08-20 14:39:29 +0600

    fix the order details page internal server error issues

--- server/client/src/components/Manager/UserDashboard/OrderDetails/OrderDetails.jsx
+++ server/client/src/components/Manager/UserDashboard/OrderDetails/OrderDetails.jsx
@@ -35,93 +35,95 @@ const OrderDetails = () => {
         <h2>{error}</h2>
       ) : (
         <>
-          <div className="orderDetails grid container-div">
-            <div className="orderDetails__container grid">
-              <h2>
-                Order Id: <span>{order && order._id}</span>
-              </h2>
-              <div className="orderDetails__container__box">
-                <h3>Shipping Info</h3>
-                <div>
-                  <p>Name:</p>
-                  <span>{order.user && order.user.name}</span>
-                </div>
-                <div>
-                  <p>Phone:</p>
-                  <span>
-                    0{order.shippingInfo && order.shippingInfo.phoneNo}
-                  </span>
-                </div>
+          {order && (
+            <div className="orderDetails grid container-div">
+              <div className="orderDetails__container grid">
+                <h2>
+                  Order Id: <span>{order && order._id}</span>
+                </h2>
+                <div className="orderDetails__container__box">
+                  <h3>Shipping Info</h3>
+                  <div>
+                    <p>Name:</p>
+                    <span>{order.user && order.user.name}</span>
+                  </div>
+                  <div>
+                    <p>Phone:</p>
+                    <span>
+                      0{order.shippingInfo && order.shippingInfo.phoneNo}
+                    </span>
+                  </div>
 
-                <div>
-                  <p className="address">Address:</p>
-                  <span>
-                    {order.shippingInfo &&
-                      `${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.pinCode}, ${order.shippingInfo.country}`}
-                  </span>
+                  <div>
+                    <p className="address">Address:</p>
+                    <span>
+                      {order.shippingInfo &&
+                        `${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.pinCode}, ${order.shippingInfo.country}`}
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <div className="orderDetails__container__box">
-                <h3>Payment</h3>
-                <div>
-                  <p
-                    className={
-                      order.paymentInfo &&
+                <div className="orderDetails__container__box">
+                  <h3>Payment</h3>
+                  <div>
+                    <p
+                      className={
+                        order.paymentInfo &&
+                        order.paymentInfo.status === "succeeded"
+                          ? "greenColor"
+                          : "redColor"
+                      }
+                    >
+                      {order.paymentInfo &&
                       order.paymentInfo.status === "succeeded"
-                        ? "greenColor"
-                        : "redColor"
-                    }
-                  >
-                    {order.paymentInfo &&
-                    order.paymentInfo.status === "succeeded"
-                      ? "PAID"
-                      : "NOT PAID"}
-                  </p>
+                        ? "PAID"
+                        : "NOT PAID"}
+                    </p>
+                  </div>
+
+                  <div>
+                    <p>Amount:</p>
+                    <span>
+                      ${order.totalPrice && order.totalPrice.toFixed(2)}
+                    </span>
+                  </div>
                 </div>
 
-                <div>
-                  <p>Amount:</p>
-                  <span>
-                    ${order.totalPrice && order.totalPrice.toFixed(2)}
-                  </span>
-                </div>
-              </div>
-
-              <div className="orderDetails__container__box">
-                <h3>Order Status</h3>
-                <div>
-                  <p
-                    className={
-                      order.orderStatus && order.orderStatus === "Delivered"
-                        ? "greenColor"
-                        : "redColor"
-                    }
-                  >
-                    {order.orderStatus && order.orderStatus}
-                  </p>
+                <div className="orderDetails__container__box">
+                  <h3>Order Status</h3>
+                  <div>
+                    <p
+                      className={
+                        order.orderStatus && order.orderStatus === "Delivered"
+                          ? "greenColor"
+                          : "redColor"
+                      }
+                    >
+                      {order.orderStatus && order.orderStatus}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="orderDetails__cartItems">
-              <h3>Order Items:</h3>
-              <div className="orderDetails__cartItems__container">
-                {order.orderItems &&
-                  order.orderItems.map((item) => (
-                    <div key={item.product}>
-                      <img src={item.image} alt="Product" />
-                      <Link to={`/product/${item.product}`}>
-                        {item.name}
-                      </Link>{" "}
-                      <span>
-                        {item.quantity} X ${item.price} ={" "}
-                        <b>${item.price * item.quantity}</b>
-                      </span>
-                    </div>
-                  ))}
+              <div className="orderDetails__cartItems">
+                <h3>Order Items:</h3>
+                <div className="orderDetails__cartItems__container">
+                  {order.orderItems &&
+                    order.orderItems.map((item) => (
+                      <div key={item.product}>
+                        <img src={item.image} alt="Product" />
+                        <Link to={`/product/${item.product}`}>
+                          {item.name}
+                        </Link>{" "}
+                        <span>
+                          {item.quantity} X ${item.price} ={" "}
+                          <b>${item.price * item.quantity}</b>
+                        </span>
+                      </div>
+                    ))}
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </>
       )}
     </>
