827735457dc2c6ebd5a5feb39f11656abff4e092
mdhossin <mdsahadathossin778@gmail.com>
2022-11-21 08:44:49 +0600

    tested

---
+++ server/client/src/pages/test.jsx
@@ -0,0 +1,9 @@
+const Test = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum, eum provident sit, vitae distinctio explicabo dolorem quo, quisquam magnam praesentium deserunt ipsam libero saepe sed fuga. Distinctio, aut repudiandae.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat expedita architecto accusamus esse alias repudiandae. Vitae at praesentium sint mollitia dolor doloribus dolores et? Repellendus ipsam labore doloremque molestias cumque.
+        </div>
+    )
+}
